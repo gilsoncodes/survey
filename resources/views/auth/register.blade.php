9e5a969b1828e63d8b27e8efc88eb3923fca5fa3
Gilson Reis <reis.gilson@gmail.com>
2021-05-17 01:28:17 -0400

    5th staging deployment - fix some bugs at local

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,28 +13,32 @@
 
               <div>
                   <x-jet-label for="name" value="{{ __('Name') }}" />
-                  <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name', 'gilson')" required autofocus autocomplete="name" />
+                  <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
               </div>
 
                 <input type="hidden" name="locale" value="{{ app()->getLocale() }}">
 
               <div class="mt-4">
                   <x-jet-label for="email" value="{{ __('Email') }}" />
-                  <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', time(). '@gmail.com')" required />
+                  <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                  {{-- :value="old('email', time(). '@gmail.com')" --}}
               </div>
               <div class="mt-4">
                   <x-jet-label for="password" value="{{ __('Password') }}" />
-                  <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" value="Aaaa!111" />
+                  <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password"  />
+                  {{-- value="Aaaa!111" --}}
               </div>
 
               <div class="mt-4">
                   <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                  <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" value="Aaaa!111"/>
+                  <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                  {{-- value="Aaaa!111" --}}
               </div>
 
               <div class="mt-4">
                   <x-jet-label for="code" value="{{ __('Authorization Code') }}" />
-                  <x-jet-input id="code" class="block mt-1 w-full" type="text" name="code" :value="old('code', 'JobApplication')"  />
+                  <x-jet-input id="code" class="block mt-1 w-full" type="text" name="code" :value="old('code')"  />
+                  {{-- , 'JobApplication' --}}
                   <div class="mt-2 text-xs text-center" >
                     <a href="{{ route('contact', [ 'lang' => app()->getLocale()]) }}" class="underline text-gray-600 hover:text-gray-900">
                       {{ __('Contact us') }}</a>
@@ -46,8 +50,8 @@
                   <div class="mt-4">
                       <x-jet-label for="terms">
                           <div class="flex items-center">
-                              <x-jet-checkbox name="terms" id="terms"  checked />
-
+                              <x-jet-checkbox name="terms" id="terms"   />
+                                {{-- checked --}}
                               <div class="ml-2">
                                   {!! __('I agree to the :terms_of_service and :privacy_policy', [
                                           'terms_of_service' => '<a target="_blank" href="'.route('terms.show', [ 'lang' => app()->getLocale()]).'" class="underline text-sm text-gray-600 hover:text-gray-900">'.__('Terms of Service').'</a>',
