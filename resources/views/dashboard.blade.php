94784f90723c5c395b8b1a98505df5cd04793e79
Gilson Reis <reis.gilson@gmail.com>
2021-07-07 16:14:51 -0400

    deployment test19

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,8 @@
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{-- {{ __('Dashboard') }} --}}
-            Dashboard8
+            Dashboard9
+            
 
         </h2>
     </x-slot>
