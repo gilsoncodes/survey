5dbf4c923d38b5955d7fa93c186471cdbdd75229
Gilson Reis <reis.gilson@gmail.com>
2021-07-08 02:03:26 -0400

    deployment test30

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout pagetitle="{{ __('Home') }}">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-16">
-            Home 9
-
-
+            Home 10
         </h2>
     </x-slot>
 
