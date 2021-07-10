353997a12c63029f1cf14e9b6f616bbf53e54c84
Gilson Reis <reis.gilson@gmail.com>
2021-07-09 22:53:19 -0400

    multilanguage is done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout pagetitle="{{ __('Home') }}">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight mt-16">
-            Home
+            Home Page
         </h2>
     </x-slot>
 
