8dba7070ef8b3544b41a2e775313fd305bd35c0c
gilsoncodes <132647359+gilsoncodes@users.noreply.github.com>
2023-05-05 13:47:30 -0400

    Delete navigation-menu.blade.php

--- navigation-menu.blade.php
+++
@@ -1,8 +0,0 @@
-<nav x-data="{ open: false, top: true  }" class="pb-2 fixed top-0 left-0 z-20 w-full bg-white border-b border-gray-100"><!-- fixed -->
-	<x-top-navigation />
-    <!-- Primary Navigation Menu -->
-    <div class="max-w-7xl justify-center text-center text-4xl font-bold mx-auto px-4 md:px-2 lg:px-8">
-    		{{ __('Corporal Punishment Survey') }}
-    </div>
-
-</nav>
